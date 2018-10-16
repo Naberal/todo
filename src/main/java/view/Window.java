d9c91732b79db4436609479900f705fd7c89b00e
naberal <hmelarvova@gmail.com>
2018-10-16 18:39:14 +0300

    add controller

--- src/main/java/view/Window.java
+++ src/main/java/view/Window.java
@@ -1,8 +1,7 @@
 package view;
 
+import controller.Controller;
 import model.Item;
-import service.Service;
-import service.ServiceImp;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,47 +12,47 @@ import java.util.List;
 public class Window {
     private JFrame frame = new JFrame("todo");
     private Container contentPane = frame.getContentPane();
-    Service<Item> service = new ServiceImp();
-    List<Item> all = service.getAll();
+    private Controller controller = new Controller();
+    private List<Item> all = controller.getAll();
 
     public void frame() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
-        table();
         add();
+        for (int i = 0; i < all.size(); i++) {
+            table(all.get(i));
+        }
         frame.setVisible(true);
         frame.setLocation(400, 100);
         frame.setSize(500, 500);
     }
 
-    private void table() {
-        for (int i = 0; i < all.size(); i++) {
-            Item item = all.get(i);
-            JTextField textField = new JTextField(item.getItem());
-            textField.setAutoscrolls(true);
-            JButton update = new JButton("update");
-            update.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    item.setItem(textField.getText());
-                    service.update(item);
+    private void table(Item item) {
+        JTextField textField = new JTextField(item.getItem());
+        textField.setAutoscrolls(true);
+        JButton update = new JButton("update");
+        update.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                item.setItem(textField.getText());
+                boolean update = controller.update(item);
+                if (update) {
+                    textField.setText(item.getItem());
                 }
-            });
-            JButton delete = new JButton("delete");
-            delete.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    service.delete(item);
-                    contentPane.remove(textField);
-                    contentPane.remove(update);
-                    contentPane.remove(delete);
-                    frame.invalidate();
-                    frame.validate();
-                    frame.repaint();
-                }
-            });
-            contentPane.add(textField);
-            contentPane.add(update);
-            contentPane.add(delete);
-        }
+            }
+        });
+        JButton delete = new JButton("delete");
+        delete.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                controller.delete(item);
+                contentPane.remove(textField);
+                contentPane.remove(update);
+                contentPane.remove(delete);
+                updateFrame();
+            }
+        });
+        contentPane.add(textField);
+        contentPane.add(update);
+        contentPane.add(delete);
     }
 
     private void add() {
@@ -64,16 +63,21 @@ public class Window {
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Item item = new Item(textField.getText());
-                service.save(item);
-                contentPane.removeAll();
-                table();
-                add();
-                frame.invalidate();
-                frame.validate();
-                frame.repaint();
+                boolean save = controller.save(item);
+                textField.setText("Write new item");
+                if (save) {
+                    table(item);
+                }
+                updateFrame();
             }
         });
         contentPane.add(textField);
         contentPane.add(button);
+    }
+
+    private void updateFrame() {
+        frame.invalidate();
+        frame.validate();
+        frame.repaint();
     }
 }
